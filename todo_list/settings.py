8d6ec79993e1e7bc78e18111bd9faf330f22c164
dipesh0078 <thapadipesh2058@gmai.com>
2024-04-20 15:03:27 +0545

    full backend

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+LOGIN_URL='login' # IMPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP
 STATIC_URL = 'static/'
 
 # Default primary key field type
